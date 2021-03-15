8a69033a7927eef5693c460741db36544d9a7d3f
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-03-15 14:33:28 -0300

    Save Options Menu added

--- app/src/main/java/com/projects/todolist/activity/AddToDoActivity.java
+++ app/src/main/java/com/projects/todolist/activity/AddToDoActivity.java
@@ -1,8 +1,11 @@
 package com.projects.todolist.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.projects.todolist.R;
 
@@ -12,5 +15,22 @@ public class AddToDoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_do);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add_todo, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemSave:
+                break;
+            default:
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
